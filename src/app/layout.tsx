62a30e26ac4ddee602806863dd12f53146078930
wpsadi <wpsadi@outlook.com>
2025-02-12 15:42:47 +0530

    init

---
+++ src/app/layout.tsx
@@ -0,0 +1,47 @@
+
+import "./globals.css";
+
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+
+import { Toaster } from "@/components/ui/toaster"
+
+import Providers from "./provider";
+
+
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "TrajectoryBook",
+  description: "Your alll in one tool",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <Providers>
+
+        <Toaster />
+        {children}
+        </Providers>
+        
+      </body>
+    </html>
+  );
+}
---
+++ src/app/provider.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Providers({
+    children
+}:{
+    children: React.ReactNode
+}) {
+  return (
+    <>
+        {children}
+    </>
+  )
+}
+
+export default Providers
